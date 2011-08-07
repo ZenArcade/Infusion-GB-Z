61c5a3aaba99ea5d8683070b7f664a0f77598c84
Brian Edwards <bedwablackburn@gmail.com>
2011-08-06 23:15:29 -0500

    working UV, one step at a time

--- arch/arm/mach-s5pv210/cpu-freq_c110b.c
+++ arch/arm/mach-s5pv210/cpu-freq_c110b.c
@@ -60,6 +60,17 @@ static struct cpufreq_frequency_table freq_table[] = {
 	{0, CPUFREQ_TABLE_END},
 };
 
+extern int exp_UV_mV[6];
+unsigned int freq_uv_table[6][3] = {
+	//frequency, stock voltage, current voltage
+	{1200000, 1275, 1275},
+	{1000000, 1275, 1275},
+	{800000, 1200, 1250},
+	{400000, 1050, 1050},
+	{200000, 950, 950},
+	{100000, 950, 950}
+}; 
+
 struct s5pv210_dvs_conf {
 	unsigned long       arm_volt;   /* uV */
 	unsigned long       int_volt;   /* uV */
@@ -506,7 +517,8 @@ static int s5pv210_cpufreq_target(struct cpufreq_policy *policy,
 	if (s3c_freqs.freqs.new == s3c_freqs.freqs.old && !first_run)
 		goto out;
 
-	arm_volt = dvs_conf[index].arm_volt;
+	arm_volt = (dvs_conf[index].arm_volt - (exp_UV_mV[index]*1000));
+	freq_uv_table[index][2] = (int) arm_volt / 1000;
 	int_volt = dvs_conf[index].int_volt;
 
 	/* New clock information update */
@@ -689,7 +701,8 @@ static int s5pv210_cpufreq_target(struct cpufreq_policy *policy,
 	memcpy(&s3c_freqs.old, &s3c_freqs.new, sizeof(struct s3c_freq));
 	cpufreq_debug_printk(CPUFREQ_DEBUG_DRIVER, KERN_INFO,
 			"cpufreq: Performance changed[L%d]\n", index);
-	previous_arm_volt = dvs_conf[index].arm_volt;
+	previous_arm_volt = (dvs_conf[index].arm_volt - (exp_UV_mV[index]*1000));
+	freq_uv_table[index][2] = (int) previous_arm_volt / 1000;
 
 	if (first_run)
 		first_run = false;
@@ -733,7 +746,8 @@ static int s5pv210_cpufreq_resume(struct cpufreq_policy *policy)
 	memcpy(&s3c_freqs.old, &clk_info[level],
 			sizeof(struct s3c_freq));
 	previous_arm_volt = dvs_conf[level].arm_volt;
-
+	previous_arm_volt = (dvs_conf[level].arm_volt - (exp_UV_mV[level]*1000));
+	freq_uv_table[level][2] = (int) previous_arm_volt / 1000;
 	return ret;
 }
 #endif
@@ -800,7 +814,8 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
 
 	memcpy(&s3c_freqs.old, &clk_info[level],
 			sizeof(struct s3c_freq));
-	previous_arm_volt = dvs_conf[level].arm_volt;
+	previous_arm_volt = (dvs_conf[level].arm_volt - (exp_UV_mV[level]*1000));
+	freq_uv_table[level][2] = (int) previous_arm_volt / 1000;
 
 #ifdef CONFIG_DVFS_LIMIT
 	for(i = 0; i < DVFS_LOCK_TOKEN_NUM; i++)
